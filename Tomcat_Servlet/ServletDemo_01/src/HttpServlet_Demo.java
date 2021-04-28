675d106d7d9cad257cd6621379048c04c4048614
域哥哥 <linyu3488755@icloud.com>
2021-04-29 01:22:51 +0800

    no message

--- Tomcat_Servlet/ServletDemo_01/src/HttpServlet_Demo.java
+++ Tomcat_Servlet/ServletDemo_01/src/HttpServlet_Demo.java
@@ -11,33 +11,44 @@ import java.util.Enumeration;
 @WebServlet("/HttpServlet_Demo")
 public class HttpServlet_Demo extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //获取请求体数据，注意post才有请求体
 
+        System.out.println(req.getContentType());
         //字符输入流
-        BufferedReader br = request.getReader();
+        BufferedReader br = req.getReader();
 
         //字节输入流，用于文件上传
 //        ServletInputStream
-
+        System.out.println("doo post");
         String line = null;
+        System.out.println(br.readLine());
         while ( (line = br.readLine()) != null ) {
             System.out.println(line);
         }
-        request.getParameter("");
+       System.out.println(req.getParameter("username"));
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(request, resp);
 
+        // *获取请求行数据 (有更好的方式获取)
         System.out.println(request.getMethod());//请求方法
+        System.out.println("=============请求方法==============");
         System.out.println(request.getQueryString());//请求参数
+        System.out.println("=============请求参数==============");
         System.out.println(request.getContextPath());//请求虚拟目录
+        System.out.println("=============请求虚拟目录==============");
         System.out.println(request.getRequestURI());//统一资源标识符
+        System.out.println("============统一资源标识符===============");
         System.out.println(request.getRequestURL());//统一资源定位符
+        System.out.println("=============统一资源定位符==============");
         System.out.println(request.getProtocol());//获取协议以及其版本
+        System.out.println("============获取协议以及其版本===============");
         System.out.println(request.getRemoteAddr());//获取客户机IP
+        System.out.println("============/获取客户机IP===============");
+
 
         // *获取请求头数据
         Enumeration<String> headerNames = request.getHeaderNames();
--- Tomcat_Servlet/ServletDemo_01/web/index.jsp
+++ Tomcat_Servlet/ServletDemo_01/web/index.jsp
@@ -10,7 +10,12 @@
   <head>
     <title>$Title$</title>
   </head>
-  <body>
     Servlet demo 01
+  <body>
+  <form action="/HttpServlet_Demo" method="post" >
+    <input type="username" placeholder="账号"><br>
+    <input type="password" placeholder="密码"><br>
+    <input type="submit" value="注册">
+  </form>
   </body>
 </html>
